cbb2599e4401e8c5ed80776ef8695697fe85b2f9
ExoticAnt <juan.manuel.sardon@gmail.com>
2021-05-24 14:51:08 -0300

    Discrete models added (generator, PVGen and WTGen).

--- ModelWriters/Resources/PV.py
+++ ModelWriters/Resources/PV.py
@@ -1,6 +1,31 @@
-from .BaseGen import Generator
+from .BaseGen import Generator, DiscreteGenerator
+
+def solar_output(model_status={}):
+    #modelo sencillo
+    #usando los datos de radiacion solar entregada por el modelo
+    #considero el valor normal 1000 W/m2
+    res = 0.0
+    if 'I' in model_status:
+        I = model_status['I']
+        if 0.0 <= I and I < 1200:
+            res = I / 1000.0
+        else:
+            raise ValueError("Solar radiation outside model range")
+    else:
+        raise ValueError("Solar radiation not defined")
+    return res
+
+
 
 class PVGenerator(Generator):
+        
+    def __init__(self, name):
+        super().__init__(name)
+        
+        self['pa_pu'] = solar_output
+        
+        
+class PVGeneratorDiscrete(DiscreteGenerator):
     
     def solar_output(self, model_status={}):
         #modelo sencillo
@@ -20,5 +45,4 @@ class PVGenerator(Generator):
     def __init__(self, name):
         super().__init__(name)
         
-        self['pa_pu'] = self.solar_output
-        
+        self['pa_pu'] = solar_output
